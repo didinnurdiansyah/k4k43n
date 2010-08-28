811aca5c5ec8d60e22ab2b013ee1cc54afc24a44
Ahmad Tanwir <ata@javan.co.id>
2010-08-28 22:24:59 +0700

    bla..bla..

--- protected/tests/unit/MahasiswaTest.php
+++ protected/tests/unit/MahasiswaTest.php
@@ -2,30 +2,14 @@
 
 class MahasiswaTest extends CDbTestCase
 {
-    /*
     public $fixtures=array(
         'mahasiswas'=>'Mahasiswa',
     );
 
-    public function testCreate()
+    
+    public function testCreate($mahasiswas)
     {
         
     }
-    */
-    
-    public function testRegisterAll()
-    {
-        $mahasiswa = Mahasiswa::model()->findAll();
-        foreach ($mahasiswa as $m) {
-            $m->password = 'test';
-            $m->registered = 1;
-            $m->email = 'test@email.com';
-            $m->jenisKelamin = rand(0,1);
-            $m->jurusan = rand(2,84);
-            $m->save(false);
-        }
-        $this->assertTrue(true,true);
-    }
-    
     
 }
